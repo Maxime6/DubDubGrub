e882861f08a21a7bf8e46ca5191610f542c4734b
Max6me <maxime.tanter@hotmail.fr>
2024-05-29 12:37:03 +0200

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Maxime Tanter on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
